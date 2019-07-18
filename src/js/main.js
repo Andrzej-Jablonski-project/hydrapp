b0ec4b931e25fee7b6ee3c96387355ac3e7a8873
Andrzej Jabłoński <andrzej@audioszum.pl>
2019-07-18 23:19:01 +0200

    feat: The function of adding, removing glasses of water.

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,34 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const addButton = document.querySelector('.hydrapp__button--add-js');
+const removeButton = document.querySelector('.hydrapp__button--remove-js');
+const info = document.querySelector('.hydrapp__cunter--js');
+const key = new Date().toISOString().slice(0, 10);
+const status = localStorage.getItem(key);
 
-console.log(`Hello world!`);
+let counter;
 
+if (!status) {
+  info.textContent = 0;
+  localStorage.setItem(key, '0');
+} else {
+  counter = status;
+  info.textContent = counter;
+}
+counter = status;
+addButton.addEventListener('click', () => {
+  if (localStorage.getItem(key) !== "99") {
+    localStorage.setItem(key, ++counter);
+    info.textContent = counter;
+  }
+});
+removeButton.addEventListener('click', () => {
+  if (localStorage.getItem(key) !== "0") {
+    localStorage.setItem(key, --counter);
+    info.textContent = counter;
+  };
+});
 
+const archive = JSON.stringify(localStorage);
+console.log(archive);
