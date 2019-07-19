f059d9862338bec81c15ae3dcb18abf43c5a3024
Andrzej Jabłoński <andrzej@audioszum.pl>
2019-07-19 15:17:01 +0200

    feat: Adds history page

--- src/js/main.js
+++ src/js/main.js
@@ -21,29 +21,37 @@ const removeButton = document.querySelector('.hydrapp__button--remove-js');
 const info = document.querySelector('.hydrapp__cunter--js');
 const key = new Date().toISOString().slice(0, 10);
 const status = localStorage.getItem(key);
-
 let counter;
 
-if (!status) {
-  info.textContent = 0;
-  localStorage.setItem(key, '0');
-} else {
-  counter = status;
-  info.textContent = counter;
-}
-counter = status;
-addButton.addEventListener('click', () => {
-  if (localStorage.getItem(key) !== "99") {
-    localStorage.setItem(key, ++counter);
+if (addButton && removeButton && info) {
+  if (!status) {
+    info.textContent = 0;
+    localStorage.setItem(key, '0');
+  } else {
+    counter = status;
     info.textContent = counter;
   }
-});
-removeButton.addEventListener('click', () => {
-  if (localStorage.getItem(key) !== "0") {
-    localStorage.setItem(key, --counter);
-    info.textContent = counter;
-  };
-});
-
-const archive = JSON.stringify(localStorage);
-console.log(archive);
+  counter = status;
+  addButton.addEventListener('click', () => {
+    if (localStorage.getItem(key) !== "99") {
+      localStorage.setItem(key, ++counter);
+      info.textContent = counter;
+    }
+  });
+  removeButton.addEventListener('click', () => {
+    if (localStorage.getItem(key) !== "0") {
+      localStorage.setItem(key, --counter);
+      info.textContent = counter;
+    };
+  });
+}
+const table = document.querySelector('.history-table__data--js');
+if (table) {
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+    console.log(key);
+    console.log(value);
+    table.innerHTML += `<tr class="table__tr"><th class="table__th">${key}</th><th class="table__th">${value}</th></tr>`;
+  }
+}
